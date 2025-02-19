77c28069f91d6a38d9a1f04c9640bc978140f523
majoralpaca984 <eleonora.troiani84@gmail.com>
2025-02-19 12:04:11 +0100

    eliminazione codice in piu html e nuova pagina js dettagli

---
+++ detailsPage.js
@@ -0,0 +1,9 @@
+const authorBox = document.getElementById('authorBox'); //da dichiare in html in authorBox
+
+
+
+
+
+const query = window.location.search;
+const params = new URLSearchParams(query)
+
